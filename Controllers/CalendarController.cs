897dd6a890465d40fb2914eaa1b52c89344f7281
Anton Matusevich <rodgerfunny@gmail.com>
2025-09-10 22:23:18 +0300

    1.1.1

--- Controllers/CalendarController.cs
+++ Controllers/CalendarController.cs
@@ -20,11 +20,9 @@ namespace DP_BurLida.Controllers
             var targetYear = year ?? currentDate.Year;
             var targetMonth = month ?? currentDate.Month;
 
-            // Ограничиваем год разумными пределами
             if (targetYear < 2020 || targetYear > 2030)
                 targetYear = currentDate.Year;
 
-            // Ограничиваем месяц
             if (targetMonth < 1 || targetMonth > 12)
                 targetMonth = currentDate.Month;
 
@@ -40,19 +38,15 @@ namespace DP_BurLida.Controllers
                 Month = month
             };
 
-            // Получаем первый день месяца
             var firstDayOfMonth = new DateTime(year, month, 1);
             var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
 
-            // Получаем первый день недели для отображения (понедельник)
             var firstDayOfWeek = GetFirstDayOfWeek(firstDayOfMonth);
             var lastDayOfWeek = GetLastDayOfWeek(lastDayOfMonth);
 
-            // Получаем все заказы за период
             var orders = await _orderService.GetAllAsync();
             var ordersInPeriod = orders.Where(o => o.CreationTimeData >= firstDayOfWeek && o.CreationTimeData <= lastDayOfWeek).ToList();
 
-            // Создаем дни календаря
             var currentDate = firstDayOfWeek;
             while (currentDate <= lastDayOfWeek)
             {
@@ -76,7 +70,6 @@ namespace DP_BurLida.Controllers
         private DateTime GetFirstDayOfWeek(DateTime date)
         {
             var dayOfWeek = (int)date.DayOfWeek;
-            // Преобразуем воскресенье (0) в 7 для правильного расчета
             if (dayOfWeek == 0) dayOfWeek = 7;
             return date.AddDays(-(dayOfWeek - 1));
         }
@@ -84,18 +77,8 @@ namespace DP_BurLida.Controllers
         private DateTime GetLastDayOfWeek(DateTime date)
         {
             var dayOfWeek = (int)date.DayOfWeek;
-            // Преобразуем воскресенье (0) в 7 для правильного расчета
             if (dayOfWeek == 0) dayOfWeek = 7;
             return date.AddDays(7 - dayOfWeek);
-        }
-
-        public async Task<IActionResult> DayDetails(DateTime date)
-        {
-            var orders = await _orderService.GetAllAsync();
-            var dayOrders = orders.Where(o => o.CreationTimeData.Date == date.Date).ToList();
-            
-            ViewBag.SelectedDate = date;
-            return View(dayOrders);
         }
     }
 }

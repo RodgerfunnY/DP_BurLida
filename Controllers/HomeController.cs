13469028f02d446a2b531c615630b69e713285fd
Anton Matusevich <rodgerfunny@gmail.com>
2025-09-11 00:18:13 +0300

    1.4

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using DP_BurLida.Models;
+using DP_BurLida.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DP_BurLida.Controllers
@@ -7,14 +8,28 @@ namespace DP_BurLida.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IOrderServices _orderService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IOrderServices orderService)
         {
             _logger = logger;
+            _orderService = orderService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var allOrders = await _orderService.GetAllAsync();
+            
+            var statistics = new
+            {
+                TotalOrders = allOrders.Count,
+                WaitingOrders = allOrders.Count(o => o.Status == "Ожидание"),
+                InWorkOrders = allOrders.Count(o => o.Status == "В работе"),
+                RepairOrders = allOrders.Count(o => o.Status == "Ремонт"),
+                CompletedOrders = allOrders.Count(o => o.Status == "Завершен")
+            };
+            
+            ViewBag.Statistics = statistics;
             return View();
         }
 
